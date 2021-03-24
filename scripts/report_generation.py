8e249f88c2a0a70aa885ed17cb5ffe96fbab8725
AnushaSrirenganathan <anusha25cse@gmail.com>
2021-03-23 22:47:40 -0400

    Update report_generation.py

--- scripts/report_generation.py
+++ scripts/report_generation.py
@@ -25,12 +25,12 @@ dbConnection = engine.connect()
 
 
 outputDir = "./"
-yesterday = datetime.date.today() - datetime.timedelta(days=2)
-#admin1_daily(outputDir, str(yesterday), engine)
-#admin1_summary(outputDir, str(yesterday), engine)
+yesterday = datetime.date.today() - datetime.timedelta(days=1)
+admin1_daily(outputDir, str(yesterday), engine)
+admin1_summary(outputDir, str(yesterday), engine)
 
-#admin2_daily(outputDir, str(yesterday), engine)
-#admin2_summary(outputDir, str(yesterday), engine)
+admin2_daily(outputDir, str(yesterday), engine)
+admin2_summary(outputDir, str(yesterday), engine)
 
 global_daily(outputDir, str(yesterday), engine)
 global_summary(outputDir, str(yesterday), engine)
