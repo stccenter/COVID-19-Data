d149ff891ba859069b0e31a374701dd5d8395955
AnushaSrirenganathan <anusha25cse@gmail.com>
2021-03-30 21:24:19 -0400

    Update admin_validation.py

--- scripts/admin_validation.py
+++ scripts/admin_validation.py
@@ -23,9 +23,9 @@ dbConnection = engine.connect()
 output_path = './'
 yesterday = datetime.date.today() - datetime.timedelta(days=2)
 url1 = "https://raw.githubusercontent.com/stevenliuyi/covid19-csv/master/csv/admin1_" + \
-    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H6BZ5FFIYSTRDXWB5LANOLRW"
+    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H4NLEJXK75GZAB7ZBTANUE22"
 url2 = "https://raw.githubusercontent.com/stevenliuyi/covid19-csv/master/csv/admin2_" + \
-    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5HYOXCYJIESZAUNIXK3ANOLPG"
+    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H3DYZFGDW6QESVSLODANUEZA"
 
 
 GITHUB_USER = os.getenv('USERNAME')
