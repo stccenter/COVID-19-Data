4a61e638bb10a933ee4950cbf173466cdb269217
Daler Khamidov <44105961+DalerKhamidov@users.noreply.github.com>
2021-07-15 15:52:40 -0400

    Update admin_validation.py

--- scripts/admin_validation.py
+++ scripts/admin_validation.py
@@ -24,9 +24,9 @@ dbConnection = engine.connect()
 output_path = './'
 yesterday = datetime.date.today() - datetime.timedelta(days=2)
 url1 = "https://raw.githubusercontent.com/stevenliuyi/covid19-csv/master/csv/admin1_" + \
-    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H2BXOOUHMMZYXRWKXTA7CQUI"
+    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H72IKZGMZELL5S5MG3A7HGAI"
 url2 = "https://raw.githubusercontent.com/stevenliuyi/covid19-csv/master/csv/admin2_" + \
-    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5HY4AP3P6ZJ7GBEAT63A7CQ3E"
+    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5HZCT5RMVCODDRUAKZDA7HGB6"
 
 
 GITHUB_USER = os.getenv('USERNAME')
