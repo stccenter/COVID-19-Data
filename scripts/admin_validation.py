3703a0725355726d29c8accc59170f2fdcebcd8e
Seren Smith <43047721+Seren-Smith@users.noreply.github.com>
2021-08-29 19:29:23 -0400

    Update admin_validation.py

--- scripts/admin_validation.py
+++ scripts/admin_validation.py
@@ -24,9 +24,9 @@ dbConnection = engine.connect()
 output_path = './'
 yesterday = datetime.date.today() - datetime.timedelta(days=2)
 url1 = "https://raw.githubusercontent.com/stevenliuyi/covid19-csv/master/csv/admin1_" + \
-    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5HYLXZCENTOLUDVIXDTBGPBXE"
+    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H5IVB5BISIYBCNYSTLBGVJHQ"
 url2 = "https://raw.githubusercontent.com/stevenliuyi/covid19-csv/master/csv/admin2_" + \
-    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5HZZ2AI2WM3ZTA5YVQ3BGPBY4"
+    yesterday.strftime("%Y-%m-%d") + ".csv?token=AFNE5H2O6WOWBLWIQCOQCKLBGVJJS"
 
 
 GITHUB_USER = os.getenv('USERNAME')
