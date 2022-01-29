e6ffd2e6e38673062b43e717645b45d8c98a14ab
Anusha Srirenganathan <anusha25cse@gmail.com>
2022-01-29 16:54:57 -0500

    Update report_generation.py

--- scripts/report_generation.py
+++ scripts/report_generation.py
@@ -25,7 +25,7 @@ dbConnection = engine.connect()
 
 outputDir = "./"
 #yesterday variable
-yesterday = datetime.date.today() - datetime.timedelta(days=29)
+yesterday = datetime.date.today() - datetime.timedelta(days=28)
 admin1_daily(outputDir, str(yesterday), engine)
 admin1_summary(outputDir, str(yesterday), engine)
 
